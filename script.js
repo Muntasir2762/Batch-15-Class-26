0742511beb2baf8fe2dfd990adeaa6a954ac5454
Munotasir2762 <muntasirpranto2762@gmail.com>
2024-09-30 23:45:24 +0600

    Js intro-variable-operator

---
+++ script.js
@@ -0,0 +1,27 @@
+console.log("This is the first class of js");
+
+
+//    var x = 50;
+   let y = 20;
+//    let x = 40;
+//    var x = 50;
+
+   let x;
+
+   x = 30;
+   x = 40;
+
+   const z = 30;
+
+//    z = 40;
+   console.log(z+x);
+
+   var result = x+y;
+
+    // console.log(result);
+
+    var rahimTk = 2000;
+    var purchase = 1000;
+
+    var remaining = rahimTk-purchase;
+    console.log(remaining);
